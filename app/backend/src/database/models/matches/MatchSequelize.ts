65046ad8ce605d0f31eca9e53658c038fe010de2
Pedro Medeiros <pedropwks@gmail.com>
2023-09-21 10:45:54 -0300

    Feat: Adicionando migration e um model para a tabela de matches

---
+++ app/backend/src/database/models/matches/MatchSequelize.ts
@@ -0,0 +1,68 @@
+import {
+  DataTypes,
+  Model,
+  InferAttributes,
+  InferCreationAttributes,
+  CreationOptional,
+} from 'sequelize';
+import db from '..';
+import TeamSequelize from '../teams/TeamSequelize';
+
+class MatchSequelize extends Model<InferAttributes<MatchSequelize>,
+InferCreationAttributes<MatchSequelize>> {
+  declare id: CreationOptional<number>;
+
+  declare homeTeamId: number;
+
+  declare homeTeamGoals: number;
+
+  declare awayTeamId: number;
+
+  declare awayTeamGoals: number;
+
+  declare inProgress: boolean;
+}
+
+MatchSequelize.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  homeTeamId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  homeTeamGoals: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  awayTeamId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  awayTeamGoals: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  inProgress: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+  },
+}, {
+  sequelize: db,
+  modelName: 'matches',
+  timestamps: false,
+  underscored: true,
+});
+
+MatchSequelize.belongsTo(TeamSequelize, { foreignKey: 'homeTeamId' });
+
+MatchSequelize.belongsTo(TeamSequelize, { foreignKey: 'awayTeamId' });
+
+TeamSequelize.hasMany(MatchSequelize, { foreignKey: 'homeTeamId' });
+
+TeamSequelize.hasMany(MatchSequelize, { foreignKey: 'awayTeamId' });
+
+export default MatchSequelize;
