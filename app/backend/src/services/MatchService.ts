ab9b5a8a8f6b8f167c1f4600a9fa2548fc4cf6d4
Pedro Medeiros <pedropwks@gmail.com>
2023-09-23 02:34:35 -0300

    Feat: Adicionando filtro a rota POST do endpoint /matches

--- app/backend/src/services/MatchService.ts
+++ app/backend/src/services/MatchService.ts
@@ -5,12 +5,15 @@ import IMatchModel from '../Interfaces/matches/IMatchModel';
 import MatchModel from '../database/models/matches/MatchModel';
 import { UpdateMatch } from '../types/UpdateMatch';
 import { CreateMatch } from '../types/CreateMatch';
+import TeamModel from '../database/models/teams/TeamModel';
+import ITeamModel from '../Interfaces/teams/ITeamModel';
 
 const messageMatchNotExist = { message: 'Partida não existe' };
 
 export default class MatchService {
   constructor(
     private matchModel: IMatchModel = new MatchModel(),
+    private teamModel: ITeamModel = new TeamModel(),
   ) { }
 
   public async getAllMatches(progress?: unknown): Promise<ServiceResponse<IMatch[]>> {
@@ -49,8 +52,20 @@ export default class MatchService {
     return { status: 'SUCCESSFUL', data: updateGoals };
   }
 
-  public async createMatch(object: CreateMatch): Promise<ServiceResponse<IMatch>> {
+  public async createMatch(object: CreateMatch): Promise<ServiceResponse<IMatch | Message>> {
+    const { homeTeamId, awayTeamId } = object;
     const create = await this.matchModel.createMatch(object);
+    const teamOne = await this.teamModel.findById(homeTeamId);
+    const teamTwo = await this.teamModel.findById(awayTeamId);
+
+    if (!teamOne || !teamTwo) {
+      return { status: 'NOT_FOUND', data: { message: 'There is no team with such id!' } };
+    }
+
+    if (teamOne.teamName === teamTwo.teamName) {
+      return { status: 'UNPROCESSABLE',
+        data: { message: 'It is not possible to create a match with two equal teams' } };
+    }
 
     return { status: 'CREATED', data: create };
   }
